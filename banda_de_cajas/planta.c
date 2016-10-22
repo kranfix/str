7827ee1ec3ec73187f9928028a549e0d2218225c
Frank Moreno <frankmoreno1993@gmail.com>
2016-10-22 14:46:53 -0500

    banda_de_cajas: planta mejor definida

--- banda_de_cajas/planta.c
+++ banda_de_cajas/planta.c
@@ -1,25 +1,31 @@
 #include "planta.h"
 
 // Actuador del brazo X
-void runX(){
+void runX(banda_t * banda, int nivel_D){
   int i;
-  puts("    Actuando brazo X");
+  puts("   Actuando brazo X");
+  banda->casilla[0] = 1;
+  banda->nivel_D[0] = nivel_D;
   for(i = 0; i < 500000; i++);
-  puts("    Robot X terminado");
+  puts("   Robot X terminado");
 }
 
 // Actuador del brazo Y
-void runY(){
+void runY(banda_t * banda){
   int i;
   puts("   Actuando brazo Y");
+  banda->casilla[banda->n - 1] = 0;
+  banda->nivel_D[banda->n - 1] = 0;
   for(i = 0; i < 600000; i++);
   puts("   Robot Y terminado");
 }
 
 // Actuador del piston C
-void runC(){
+void runC(banda_t * banda){
   int i;
   puts("   Actuando piston C");
+  banda->casilla[banda->m] = 0;
+  banda->nivel_D[banda->m] = 0;
   for(i = 0; i < 200000; i++);
   puts("   Piston C terminado");
 }
@@ -30,10 +36,10 @@ void runBanda(banda_t * banda){
   puts("   Actuando un paso de la banda");
   for(i = banda->n-1; i > 0; i--){
     banda->casilla[i] = banda->casilla[i-1];
-    banda->nivel_D[i] = banda->nivel_D[i-1]; 
+    banda->nivel_D[i] = banda->nivel_D[i-1];
   }
   banda->casilla[0] = 0;
-  banda->nivel_D[0] = 0; 
+  banda->nivel_D[0] = 0;
 
   for(i = 0; i < 1000000; i++);
   puts("   Paso de la banda terminado");
--- banda_de_cajas/planta.h
+++ banda_de_cajas/planta.h
@@ -10,12 +10,13 @@ typedef struct {
   int m;         // posicion del sensor D
   int max_D;     // Tope del sensor D
   int * casilla; // hay caja
-  int * nivel_D; // para el sensor 
+  int * nivel_D; // para el sensor
 } banda_t;
 
-void runY();     // Actuador del brazo Y
-void runC();     // Actuador del piston C
-void runBanda(); // Actuador de la banda X
+void runX(banda_t * banda, int nivel_D);
+void runY(banda_t * banda);     // Actuador del brazo Y
+void runC(banda_t * banda);     // Actuador del piston C
+void runBanda(banda_t * banda); // Actuador de la banda
 
 /* Sensores */
 void leerSensores(banda_t * banda, int * A, int *B, int *D);
